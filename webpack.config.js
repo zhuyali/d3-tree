a7aea24ced2fea10d998ee538a14f2d38979eeac
zivyll <me@yanglonglong.com>
2017-12-20 16:39:50 +0800

    update version

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['es2015', 'latest'],
+            presets: ['env', 'es2015', 'latest'],
             plugins: [
               [
                 'import',
